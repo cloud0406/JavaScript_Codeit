1227802d3d6346361e28210bd3957f56bd44019b
김정호 <cloud0406@naver.com>
2021-07-30 22:02:40 +0900

    practice : use parameter
    * 파라미터 여러개 사용

---
+++ 파라미터/여러개의 파라미터/index.js
@@ -0,0 +1,18 @@
+function printSum(num1, num2){
+  console.log(num1 + num2);
+};
+
+printSum(10,5);
+printSum(123,4);
+printSum(50,70);
+
+function introduce(name ,birth, nationality, job){
+  console.log('안녕하세요! 반갑습니다.');
+  console.log('제 이름은 ' + name + ' 입니다.');
+  console.log('생년월일은 ' + birth + ' 이고',);
+  console.log('국적은 ' + nationality + ' 입니다.');
+  console.log('직업은 ' + job + ' 입니다.');
+  console.log('잘 부탁드립니다!');
+};
+
+introduce('김정호', '1998.04.06', '한국', '대학생')
