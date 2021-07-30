54aa406338ae3f64cbaf29d441c2cc4e8d8fa349
김정호 <cloud0406@naver.com>
2021-07-30 22:01:39 +0900

    practice : use parameter

---
+++ 파라미터/index.js
@@ -0,0 +1,14 @@
+function welcome(name){
+  console.log('안녕하세요 ' + name + '님!');
+}
+
+welcome('코드잇');
+welcome('김정호');
+
+function printSquare(x){
+  console.log(x * x);
+}
+
+printSquare(2);
+printSquare(5);
+printSquare(6);
