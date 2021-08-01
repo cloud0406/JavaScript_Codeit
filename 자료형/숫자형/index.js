baf9d247be1e824b6426fb24dc2d5b06626e32de
김정호 <cloud0406@naver.com>
2021-08-01 21:36:29 +0900

    practice : 숫자형

---
+++ 자료형/숫자형/index.js
@@ -0,0 +1,15 @@
+// 사칙연산
+console.log(1 + 8);
+console.log(6 - 7);
+console.log(3 * 2);
+console.log(5 / 2);
+
+//나머지
+console.log(7 % 3);
+
+//거듭제곱 (2의 3승)
+console.log(2 ** 3);
+
+//Math.trunc : 소수점을 없애줌 
+console.log(Math.trunc(5/2));
+
