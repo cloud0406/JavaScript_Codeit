0834e7c5a0c70ac8a05d43089e5fa23a2dc14edc
김정호 <cloud0406@naver.com>
2021-08-02 21:04:05 +0900

    practice : 불린형
    * 자료형- 불링형 응용

---
+++ 자료형/불린형/불린 응용/index.js
@@ -0,0 +1,17 @@
+// && 혹은 || 을 기준으로 좌우를 확인하고 마지막에 && 혹은 ||를 체크
+console.log(2 < 1 && 'Codeit' !== 'Codeit');
+console.log(7 !== 7 || 4 < 3);
+
+let k = 3;
+console.log(k > 4 || !(k > 2));
+
+
+
+let x = 4;
+let y = false;
+let z = 'c';
+
+console.log(x>5 && "codeit"==="codeit");
+console.log(y || !y);
+console.log("codeit" !== z+"odeit");
+console.log(x < 4 || !(y===true));
