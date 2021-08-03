a18665b7922d762133421bc7895aec86edeb797b
김정호 <cloud0406@naver.com>
2021-08-03 22:00:16 +0900

    practice : 형 변환
    * 형 변환 문제

---
+++ 자료형/형 변환/퀴즈/index.js
@@ -0,0 +1,18 @@
+// 123 - 1
+console.log(Number('1' + '2' + '3') - Number(true));
+
+// 문자열 값
+console.log(Boolean("false"));
+
+// 불린 false값
+console.log(Boolean(false));
+
+// 0
+console.log(Boolean(6 % 2));
+
+// 0 , 문자열
+console.log(Boolean(NaN) || Boolean('0'));
+
+// 'Boolean':문자열
+console.log(Boolean(typeof false));
+
