e384fb8a850db784cd628016dcaabcac32c8fd0e
김정호 <cloud0406@naver.com>
2021-08-04 21:20:56 +0900

    practice : 템플릿 문자열

---
+++ 자료형/템플릿 문자열/index.js
@@ -0,0 +1,15 @@
+let year = 2020;
+let month = 8;
+let day = 4;
+
+console.log('생년월일은 ' + year + '년 ' + month + '월 ' + day +'일 입니다.');
+
+console.log(`생년월일은 ${year}년 ${month}월 ${day}일 입니다.`); //좀 더 가독성 좋음
+
+
+let myNumber = 3;
+function getTwice(x) {
+  return x * 2;
+}
+
+console.log(`${myNumber}의 두배는 ${getTwice(myNumber)}입니다.`)
