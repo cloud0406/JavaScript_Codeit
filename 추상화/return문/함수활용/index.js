59944359acc59be2c5622057c654148a11556da7
김정호 <cloud0406@naver.com>
2021-08-06 21:31:19 +0900

    practice : return문
    * return문 이용 함수 활용

---
+++ 추상화/return문/함수활용/index.js
@@ -0,0 +1,11 @@
+// 직사각형의 넓이 구해주고 반환
+function calculateRectangleArea(width, height) {
+  return width * height;
+};
+
+
+let area1 = calculateRectangleArea(3, 4); 
+let area2 = calculateRectangleArea(5, 8); 
+let area3 = calculateRectangleArea(7, 2); 
+
+console.log(`Area1: ${area1}, Area2: ${area2}, Area3: ${area3}`);
