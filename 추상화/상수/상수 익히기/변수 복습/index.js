bf1a3dc75010dc7d396795c7cdc6e844baa23510
김정호 <cloud0406@naver.com>
2021-08-07 19:51:01 +0900

    practice : 변수
    * 변수 복습하기

---
+++ 추상화/상수/상수 익히기/변수 복습/index.js
@@ -0,0 +1,18 @@
+// 환승 구분 
+
+let adultTag = '삑!';  // 어른
+let teenagerTag = '삑삑!'; // 청소년
+let errorTag = '삑삑삑!'; // 에러
+let transferTag = '환승입니다.'; // 환승
+
+
+function tagNotification(tag){
+  console.log(tag);
+}
+
+
+tagNotification(adultTag);
+tagNotification(teenagerTag);
+tagNotification(transferTag);
+tagNotification(errorTag);
+tagNotification(adultTag);
