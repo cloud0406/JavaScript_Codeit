4580e0850778c56345fa847807742374fe241712
김정호 <cloud0406@naver.com>
2021-08-07 19:50:46 +0900

    practice : 상수
    * 상수 익히기( 상수 변경 )

---
+++ 추상화/상수/상수 익히기/index.js
@@ -0,0 +1,22 @@
+const Y = 1500;
+let y = 1000;
+
+console.log(Y);
+console.log(y);
+
+console.log('');
+
+
+
+
+const X = 1500;
+
+function myFunction() {
+  X = 1500 * 1.5; // 상수를 바꾸려 하므로 오류 발생
+  console.log(X);
+}
+
+myFunction(); 
+console.log(X);
+
+
