38e1c5e94f47e5a27a868a7d04338e207b74c492
김정호 <cloud0406@naver.com>
2021-08-08 20:57:12 +0900

    practice : if문

---
+++ 제어문/if문/index.js
@@ -0,0 +1,15 @@
+let temperature = 0;
+
+if (temperature <= 0 ){
+  console.log('물이 업니다.');
+}
+
+if (temperature > 0 ){
+  console.log('물이 얼지 않습니다.');
+}
+
+if (temperature <= 0 ){
+  console.log('물이 업니다.');
+} else {
+  console.log('물이 얼지 않습니다.');
+}
