bcf74330df6dfac4c8c253331cb40707bf04f53b
김정호 <cloud0406@naver.com>
2021-08-09 19:41:49 +0900

    practice : 반복문
    * switch문 활용

---
+++ 제어문/switch문/switch문 활용/index.js
@@ -0,0 +1,31 @@
+let VIPPrice = 15;
+let RPrice = 13;
+let SPrice = 10;
+let APrice = 8;
+
+// 각 등급에 맞는 가격을 출력하는 함수 checkPrice
+function checkPrice(grade) {
+  switch (grade) {
+    case 'VIP':
+      console.log(`${grade}석은 ${VIPPrice}만원 입니다.`);
+      break;
+    case 'R':
+      console.log(`${grade}석은 ${RPrice}만원 입니다.`);
+      break;
+    case 'S':
+      console.log(`${grade}석은 ${SPrice}만원 입니다.`);
+      break;
+    case 'A':
+      console.log(`${grade}석은 ${APrice}만원 입니다.`);
+      break;
+    default:
+      console.log('VIP, R, S, A 중에서 하나를 선택해 주세요.');
+  }
+}
+
+
+checkPrice('R');
+checkPrice('VIP');
+checkPrice('S');
+checkPrice('A');
+checkPrice('B');
