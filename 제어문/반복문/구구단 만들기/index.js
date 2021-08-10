cb1ae141d93261b45fcd8229c0399ce74577f621
김정호 <cloud0406@naver.com>
2021-08-10 21:31:27 +0900

    practice : 반복문
    * 구구단 만들기
    * for문 while문

---
+++ 제어문/반복문/구구단 만들기/index.js
@@ -0,0 +1,18 @@
+for(let i = 1; i<=9; i++){
+  for(let j =1; j<=9; j++){
+    console.log(`${i} * ${j} = ${i*j}`);
+  }
+}
+
+
+console.log(' ');
+
+let i = 1;
+while (i <= 9) {
+  let j = 1;
+  while (j <= 9) {
+    console.log(`${i} * ${j} = ${i * j}`);
+    j++;
+  }
+  i++;
+}
