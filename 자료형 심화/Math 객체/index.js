c03bd44e6fbf1b2ff5963c492f08ea7c15a21aec
김정호 <cloud0406@naver.com>
2021-08-19 22:13:01 +0900

    practice : Math 객체
    * Math.abs
    * Math.max
    * Math.min
    * Math.pow
    * Math.sqrt
    * Math.round
    * Math.floor
    * Math.ceil
    * Math.random

---
+++ 자료형 심화/Math 객체/index.js
@@ -0,0 +1,63 @@
+// Math.abs(x) : 괄호안에 숫자의 절대값을 리턴
+console.log(Math.abs(-10));
+console.log(Math.abs(10));
+
+console.log('');
+
+
+// Math.max() : 괄호 안에 숫자중 가장 큰 값을 리턴
+console.log(Math.max(2, -1, 4, 5, 0));
+
+console.log('');
+
+
+// Math.min() : 괄호 안에 숫자중 가장 작은 값을 리턴
+console.log(Math.max(2, -1, 4, 5, 0));
+
+console.log('');
+
+
+// Math.pow(x,y) : x의 y승을 계산하고 값을 리턴
+console.log(Math.pow(2, 3));
+console.log(Math.pow(5, 2));
+
+console.log('');
+
+
+// Math.sqrt(x) : x의 제곱근을 리턴(루트 씌어줌)
+console.log(Math.sqrt(25));
+console.log(Math.sqrt(49));
+
+console.log('');
+
+// Math.round(x) : x의 반올림된 값을 리턴
+console.log(Math.round(2.3));
+console.log(Math.round(2.4));
+console.log(Math.round(2.49));
+console.log(Math.round(2.5));
+console.log(Math.round(2.6));
+
+console.log('');
+
+
+// Math.floor(x) : 괄호 안의 숫자를 내림해서 나타냄 
+console.log(Math.floor(2.4));
+console.log(Math.floor(2.49));
+console.log(Math.floor(2.8));
+
+console.log('');
+
+
+// Math.ceil(x) : 괄호 안의 숫자를 올림해서 나타냄 
+console.log(Math.ceil(2.4));
+console.log(Math.ceil(2.49));
+console.log(Math.ceil(2.8));
+
+console.log('');
+
+
+// Math.random : 0이상 1미만의 값을 랜덤으로 리턴
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
